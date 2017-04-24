1db3966e63fc0079dab85ae46c1e08b54bf0beb0
klinnel <lio181@yahoo.fr>
2017-04-24 16:33:07 +0200

    ajout mon css

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,13 +10,13 @@
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<link rel="stylesheet" href="/assets/my.css">
+<link rel="stylesheet" href="{{ asset('css/my.css') }}">
 
 </head>
 
 <body>
 
-<h1><a href="contact">Contact</a> | <a href="about">About</a></h1>
+<h1 class="mynav"><a href="contact">Contact</a> | <a href="about">About</a></h1>
 
 <hr />
 
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -9,7 +9,7 @@ Contact
 
                 <div class="title">
                     <h1>Contact Me !</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    <p class="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
                 </div>

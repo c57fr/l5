3585e872add7686b6f39aa71e50a45171a0410a3
GrCOTE7 <GrCOTE7@Gmail.com>
2017-04-29 11:16:24 +0200

    Ajout laravelcollective/html

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,18 @@
+@extends ('layout.app')
+
+@section('title')
+  Create Article
+  @endSection
+
+@section('content')
+  <div class="content">
+
+    <div class="title">
+      <h1>Écrite un article</h1>
+    </div>
+
+    <form action=""></form>
+
+  </div>
+@endsection
+ 

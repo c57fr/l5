3535fac15756028bcef2c86824c3e60001ebd0b1
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-01 11:19:16 +0200

    Possibilité de changer le nom du champs

--- app/Cg7.php
+++ app/Cg7.php
@@ -9,31 +9,43 @@
 namespace App;
 
 
+use Debugbar;
 use Illuminate\Support\Facades\Validator;
 
 class Cg7 {
 
-
-  public function Maclasse() {
-
-    return 'Ma Classe';
-  }
+  //  Vous pouver changer le nom du champs ici (Ex.: nom, email, etc...)
+  protected static $nomDuChamps = 'X(NomDuChamps)';  // Ori: X(NomDuChamps)
 
 
   public static function UsageValidator() {
 
-    $messages = [
-      'min' => 'Le champs :attribute doit impérativement avoir au moins :min caractères.',
-    ];
-    $rules    = [
-      'name' => 'required|string|min:3',
-    ];
-    $input    = ['name' => 'ab'];
-    $v        = Validator::make($input, $rules, $messages)
-                         ->errors()
-                         ->all();
+    $longueurMini = 18; // Changez cette valeur pour tests
 
-    \Debugbar::addMessage($v[0], 'Test d\'utilisation du validator');
+    // En général, $input provient d'un formulaire...;
+    $input = [Cg7::$nomDuChamps => 'Que 18 caractères']; // Ori: Que 18 caractères
+
+
+    $rules = [
+      Cg7::$nomDuChamps => 'required|string|min:' . $longueurMini,
+    ];
+
+    $longChamps = strlen($input[Cg7::$nomDuChamps]);
+
+    $messages = [
+      'require' => 'Le champs est vraiment nécessaire...',
+      'string'  => 'Le champs doît être de type string',
+      'min'     => 'Le champs :attribute doit impérativement avoir au moins :min caractères.',
+    ];
+
+    $v = Validator::make($input, $rules, $messages)
+                  ->errors()
+                  ->all();
+
+    Debugbar::addMessage($v, 'Test d\'utilisation du validator');
+    Debugbar::addMessage($longChamps,
+      (' est la longueur du champs "' . (Cg7::$nomDuChamps) . '" qui contient la chaine : "' . $input[Cg7::$nomDuChamps] . '".'));
+    info('Longueur minimale recquise: ' . $longueurMini);
   }
 
 }

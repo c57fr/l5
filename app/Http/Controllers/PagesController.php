d786eb7e1b149ffcac0a689b72bb02e8b598ace9
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-01 10:10:22 +0200

    Test d'utilisation du Validator

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Validator;
 
 class PagesController extends Controller {
 
   public function Contact() {
+    $v = RegisterController::class;
+    //    $v->validator();
+
+    $messages = [
+      'min' => 'Le champs :attribute doit impérativement avoir au moins :min caractères.',
+    ];
+    $rules    = [
+      'name' => 'required|string|min:3',
+    ];
+    $input    = ['name' => 'ab'];
+    $v        = Validator::make($input, $rules, $messages)
+                         ->errors()
+                         ->all();
+
+    \Debugbar::addMessage($v[0], 'Utilisation du validator');
 
     return view('pages.contact');
   }

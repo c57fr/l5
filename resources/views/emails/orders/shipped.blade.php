e59e9744cd75fcd594ec5eeab3b3d8370e0329ea
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-04 12:36:34 +0200

    Amélioration du template

--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -1,13 +1,18 @@
 @component('mail::message')
 # Introduction
 
-The body of your message.
+Your order has been shipped!
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => $url])
+View Order
 @endcomponent
+
+Next Steps:
+
+- Track Your Order On Our Website
+- Pre-Sign For Delivery
+
 
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
-c

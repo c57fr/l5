6a9c0094e780dfce61c6d130af0d81c17e27b0fc
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-05 02:32:56 +0200

    Ajout route test

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,13 @@
 */
 
 Route::get('/', function () {
+
   return view('welcome');
 });
 
 Route::get('about', 'PagesController@about');
-
 Route::get('contact', 'PagesController@contact');
+Route::get('test', 'PagesController@test');
 
 
 Route::resource('articles', 'ArticlesController');

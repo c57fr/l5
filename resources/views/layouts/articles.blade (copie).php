c3af564a48c1e3054fdd48c52a1d0f422876d238
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-07 19:01:35 +0200

    Nouveau template

---
+++ resources/views/layouts/articles.blade (copie).php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="UTF-8"/>
+  <meta name="description" content="Etude Laravel"/>
+  <!-- <link rel="stylesheet" href="assets/css/style.min.css"> -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="{{ asset('css/my.css') }}">
+</head>
+
+<body>
+<h1 class="mynav links">
+  <a href="/">Accueil</a> | <a href="/articles">Articles</a> | <a href="/contact">Contact</a> | <a
+      href="/about">About</a>
+</h1>
+
+<hr/>
+
+<div class="container">
+
+  <h1>Page @yield('title')</h1>
+
+  @yield('content')
+
+</div>
+
+@yield('footer')
+</body>
+
+</html>

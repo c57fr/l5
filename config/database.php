b933d0ad58d622f27837aaaa57550fe39bfbdd63
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-21 08:21:25 +0200

    Simplification config

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
       'driver'      => 'mysql',
       'host'        => env('DB_HOST', '127.0.0.1'),
       'port'        => env('DB_PORT', '3306'),
-      'database'    => env('DB_DATABASE', 'admin_c57_l5'),
+      'database'    => env('DB_DATABASE', ''),
       'username'    => env('DB_USERNAME', 'root'),
       'password'    => env('DB_PASSWORD', ''),
       'unix_socket' => env('DB_SOCKET', ''),

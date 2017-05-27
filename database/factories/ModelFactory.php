212aac2c3a1ee0f2e2eb885b7ba4195a220807ac
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-27 10:26:14 +0200

    Adaptation du factory // champs nommé username

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,9 +17,9 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
   static $password;
 
   return [
-    'name'           => $faker->name,
+    'username'       => $faker->name,
     'email'          => $faker->unique()->safeEmail,
-    'password'       => $password ? : $password = bcrypt('secret'),
+    'password'       => $password ? : $password = bcrypt('pw'),
     'remember_token' => str_random(10),
   ];
 });

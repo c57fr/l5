39876f0f5a5ab739817a69bed5ad817060b9dff5
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-27 08:20:43 +0200

    Pseudo directement visible dans barre de menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,13 +8,13 @@
       <a class="blog-nav-item {{(App\C7::active('about'))?'active':''}}" href="/about">À notre sujet</a>
       <a class="blog-nav-item {{(App\C7::active('test'))?'active':''}}" href="/test">Test</a>
 
+
       @if ( Auth::check() )
 
-        <a class="blog-nav-item navbar-right" title="Logué avec:
-     {{Auth::user()->username}}" href="{{ route('logout') }}"
+        <a class="blog-nav-item navbar-right" title="Cliqué ICI pour vous déloguer" href="{{ route('logout') }}"
            onclick=" event.preventDefault();
      document.getElementById('logout-form').submit();">
-          Logout
+          {{Auth::user()->username}}
         </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">xXx
           {{ csrf_field() }}
@@ -22,6 +22,7 @@
 
       @else
         <a class="blog-nav-item navbar-right" href="/login">Login</a>
+
       @endif
 
     </nav>

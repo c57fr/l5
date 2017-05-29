ca020721b0bdb40a5f64891a81624003d4254587
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-29 15:56:36 +0200

    2 Simplification Factorisation de Article::show() et index()

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cote
- * Date: 29/05/17
- * Time: 14:19
- */
+<article>
+
+  <h1>
+    <b><a href="{{ url('articles', $article->id )}}">{{ $article->title }}</a></b>
+          <span class="links petit">
+
+          <a href="{{ url('articles/'. $article->id.'/edit' )}}">
+            <button class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></button>
+          </a>
+
+          <a href="#">
+            <button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>
+          </a>
+          <span class="petit">par {{ $article->username }}</span>
+          </span>
+
+  </h1>
+  {{--TODOLI Fctn Effacer à rendre opérationnelle--}}
+
+  {{ $article->slug }} le
+  <b>{{ $article->created_at->formatLocalized('%A %e %B %Y') }}</b> <em>( {{ $article->delai }}
+    )</em><br/><br/>
+
+  <div class="jumbotron contenu">{{ $article->body }}</div>
+
+</article>
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -29,32 +29,7 @@
           {{--Factorisation du template d'un article peut-être possible quand listing peut être de la même structure qu'un show--}}
           {{--@include('articles.article');--}}
 
-          <article>
-
-          <h1>
-          <b><a href="{{ url('articles', $article->id )}}">{{ $article->title }}</a></b>
-          <span class="links petit">
-
-          <a href="{{ url('articles/'. $article->id.'/edit' )}}">
-          <button class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></button>
-          </a>
-
-          <a href="#">
-          <button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>
-          </a>
-          <span class="petit">par {{ $article->username }}</span>
-          </span>
-
-          </h1>
-          {{--TODOLI Fctn Effacer à rendre opérationnelle--}}
-
-          {{ $article->slug }} le
-          <b>{{ $article->created_at->formatLocalized('%A %e %B %Y') }}</b> <em>( {{ $article->delai }}
-          )</em><br/><br/>
-
-          <div class="jumbotron contenu">{{ $article->body }}</div>
-
-          </article>
+          @include('articles.article');
 
           <br/>
 
